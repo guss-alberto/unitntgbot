68bea674e5fe66599078124641facf20cbaef6e7
Elia-Tomaselli <elia.tomaselli@studenti.unitn.it>
2025-07-15 16:16:28 +0200

    Refactor send_notification

--- src/unitntgbot/notification_dispatcher/main.py
+++ src/unitntgbot/notification_dispatcher/main.py
@@ -10,40 +10,53 @@ from unitntgbot.notification_dispatcher.settings import settings
 
 logger = logging.getLogger(__name__)
 
+BOT = Bot(token=settings.TELEGRAM_BOT_TOKEN)
 
 class NotificationMessage(BaseModel):
     chat_id: int
-    html_message: str
+    message: str
 
+    def __init__(self, chat_id: int, message: str):
+        super().__init__(chat_id=chat_id, message=message)
+        
+    async def send_notification(self) -> None:
+        try:
+            await BOT.send_message(chat_id=self.chat_id, text=self.message, parse_mode=ParseMode.MARKDOWN)
+        except Exception as e:
+            logger.error(f"Failed to send message to chat {self.chat_id}: {e}")
+            raise
 
-BOT = Bot(token=settings.TELEGRAM_BOT_TOKEN)
 
 
 def entrypoint() -> None:
-    asyncio.run(listen_topics())
+    asyncio.run(listen_topic(settings.KAFKA_TOPIC))
 
 
-async def listen_topics() -> None:
-    consumer = AIOKafkaConsumer(settings.KAFKA_TOPIC, bootstrap_servers=settings.KAFKA_SERVER)
+async def listen_topic(topic: str) -> None:
+    consumer = AIOKafkaConsumer(topic, bootstrap_servers=settings.KAFKA_SERVER)
 
     await consumer.start()
     try:
         async for msg in consumer:
             await handle_message(msg)
-
     finally:
         await consumer.stop()
 
 
-async def handle_message(kafka_message: ConsumerRecord) -> None:
-    try:
-        message_content = NotificationMessage.model_validate_json(kafka_message.value)
-    except ValidationError:
-        logger.error("Invalid message format: %s", kafka_message.value)
+async def handle_message(kafka_message: ConsumerRecord[bytes, bytes]) -> None:
+    if kafka_message.key is None or kafka_message.value is None:
+        logger.warning("Received message with missing key or value")
         return
 
-    await send_notification(message_content)
+    try:
+        chat_id = int.from_bytes(kafka_message.key)
+        message_text = kafka_message.value.decode("utf-8")
+        message = NotificationMessage(chat_id=chat_id, message=message_text)
+    except UnicodeDecodeError:
+        logger.error("Invalid unicode in message: %s", kafka_message.value)
+        return
+    except ValidationError:
+        logger.error("Invalid data in message: %s", kafka_message.value)
+        return
 
-
-async def send_notification(message: NotificationMessage) -> None:
-    await BOT.send_message(chat_id=message.chat_id, text=message.html_message, parse_mode=ParseMode.HTML)
+    await message.send_notification()
